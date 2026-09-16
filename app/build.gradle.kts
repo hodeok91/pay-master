7d6b90d0fd246f7a632e8002093feacfce4decaf
hodeok91 <hodeok91@gmail.com>
2026-09-16 21:23:06 +0900

    페이의 달인 v0.0.1

---
+++ app/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "kr.dalin.paymaster"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "kr.dalin.paymaster"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.0.1"
+    }
+}
---
+++ app/src/main/java/kr/dalin/paymaster/MainActivity.kt
@@ -0,0 +1,18 @@
+package kr.dalin.paymaster
+
+import android.app.Activity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val web = WebView(this)
+        web.settings.javaScriptEnabled = true
+        web.settings.domStorageEnabled = true
+        web.webViewClient = WebViewClient()
+        web.loadUrl("file:///android_asset/www/index.html")
+        setContentView(web)
+    }
+}
